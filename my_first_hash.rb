7f5bba7764de5bc2e7f8477849bf5acfec72fd84
Rabiah Hogans <hogansrabiah@gmail.com>
2020-04-01 06:55:01 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,5 +1,5 @@
 def my_hash = {"rabiah" => "student", "dominique" => "teacher"}
-
+puts my_hash
   # use the literal constructor to set the variable, my_hash, equal to a hash with key/value pairs of your choice.
 
   
